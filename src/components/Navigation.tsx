ea65ebc04e065a73a1a7c60188bf5c773cc5ea03
manselli <massimiliano.manselli@gmail.com>
2021-11-30 21:49:21 +0100

    bafg

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,15 +3,39 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 function Navigation() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  return (  
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand href="/climbing-blog/">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+      <Nav.Link href="#features"><NavLink className="nav-link" to="/climbing-blog/">Home</NavLink></Nav.Link>
+      <Nav.Link href="#features"><NavLink className="nav-link" to="/climbing-blog/about">about</NavLink></Nav.Link>
+      <Nav.Link href="#features"><NavLink className="nav-link" to="/climbing-blog/contact">contact</NavLink></Nav.Link>
+      
+    </Nav>
+    <Nav>
+      <Nav.Link href="#features"><NavLink className="nav-link" to="/climbing-blog/login">login</NavLink></Nav.Link>
+      <Nav.Link href="#features"><NavLink className="nav-link" to="/climbing-blog/signup">signup</NavLink></Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+  );
+}
+
+export default Navigation;
+
+/**
+ *   <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/climbing-blog/">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav me-auto">
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav me-auto">
             <li className="nav-item">
               <NavLink className="nav-link" to="/climbing-blog/">
                 Home
@@ -40,15 +64,13 @@ function Navigation() {
               </NavLink>
             </li>
           </ul>
-        </div>
-      </div>
-    </nav>
-  );
-}
-
-export default Navigation;
-
-/**
+    </div>
+  </div>
+</nav>
+ * 
+ * 
+ * 
+ * 
  
     <div className="navigation">
       <nav className="navbar navbar-expand navbar-dark bg-dark">
---
+++ src/components/config/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey: "AIzaSyAvMkdQjQB5Y_VwUFc2f9gWWdkBq1emIOE",
+    authDomain: "auth-development-cf731.firebaseapp.com",
+    projectId: "auth-development-cf731",
+    storageBucket: "auth-development-cf731.appspot.com",
+    messagingSenderId: "225976382629",
+    appId: "1:225976382629:web:9913ae895d9658b80fc94c"
+  };
