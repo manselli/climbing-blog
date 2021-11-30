6bf47aeac6a417ab4fb0302e59ad33cce908e1e3
manselli <massimiliano.manselli@gmail.com>
2021-11-30 21:14:59 +0100

    bafgxfi6785585

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,14 +4,14 @@ import { NavLink } from "react-router-dom";
 
 function Navigation() {
   return (
-    <div className="navigation">
-    <nav className="navbar navbar-expand navbar-dark bg-dark">
-      <div className="container">
-        <NavLink className="navbar-brand" to="/climbing-blog/">
-          React Multi-Page Website
-        </NavLink>
-        <div>
-          <ul className="navbar-nav ml-auto">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/climbing-blog/">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul className="navbar-nav me-auto">
             <li className="nav-item">
               <NavLink className="nav-link" to="/climbing-blog/">
                 Home
@@ -28,10 +28,21 @@ function Navigation() {
               </NavLink>
             </li>
           </ul>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/climbing-blog/login">
+                Login
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/climbing-blog/signup">
+               Signup
+              </NavLink>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
-  </div>
   );
 }
 
